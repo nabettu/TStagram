605f6f76dfdc301c0f8686603e3aa07b9583bb4a
tatsuakiw <tatsuaki.w@gmail.com>
2014-03-28 19:04:41 +0900

    Initial Commit

---
+++ TStagram/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  TStagram
+//
+//  Created by tatsuaki watanabe on 2014/03/28.
+//  Copyright (c) 2014年 tatsuaki watanabe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ TStagram/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  TStagram
+//
+//  Created by tatsuaki watanabe on 2014/03/28.
+//  Copyright (c) 2014年 tatsuaki watanabe. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
